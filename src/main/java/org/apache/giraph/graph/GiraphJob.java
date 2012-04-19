1e912579972e6dccb80a6fe419e3cbda28a2669c
Eugene Koontz <ekoontz@hiro-tan.org>
2012-04-19 16:30:01 -0700

    remove rage logging

--- src/main/java/org/apache/giraph/graph/GiraphJob.java
+++ src/main/java/org/apache/giraph/graph/GiraphJob.java
@@ -623,7 +623,6 @@ public class GiraphJob {
     // (DEFAULT_PING_INTERVAL)
     Client.setPingInterval(conf, 60000 * 5);
 
-    LOG.debug("EUGENE IS LOGGING!!!!");
     LOG.debug("job.getJar()=" + job.getJar());
     if (job.getJar() == null) {
       job.setJarByClass(GiraphJob.class);
